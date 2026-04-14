b6075435b70d7de4cebdf2279cfa9165629d7040
Jenny Ly <jenny@Jennys-MacBook-Air.local>
2026-04-14 16:30:59 -0700

    started shell_sort1

--- shell_sort1.py
+++ shell_sort1.py
@@ -1,2 +1,12 @@
-def shell_sort1(nums:list[int]):
-    pass
+def shell_sort1(nums:list[int], n: int):
+    gap = n/2
+    while gap > 0:
+        
+
+
+
+def swap(nums:list[int], n1: int, n2: int):
+    temp = nums[n1]
+
+    nums[n1] = nums[n2]
+    nums[n2] = temp
