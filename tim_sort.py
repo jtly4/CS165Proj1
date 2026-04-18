ba6d4e130b5497d137eafeed5d1da39f0888c1b1
Jenny Ly <jtly102@gmail.com>
2026-04-18 10:59:29 -0700

    started tim_sort

--- tim_sort.py
+++ tim_sort.py
@@ -1,2 +1,15 @@
 def tim_sort(nums: list[int]):
     pass
+
+def merge_comp(l: list[int], r: list[int]):
+    pass
+
+def run_decomp(nums: list[int]):
+    runs = []
+    n = len(nums)
+
+    for i in range(n):
+        begin = i
+
+        back = nums[i] <= nums[i + 1]
+        i += 1 
